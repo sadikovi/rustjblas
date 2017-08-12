75d7a57a170c4b6ae4eba477d179dc10d893a8ed
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2017-08-13 00:16:10 +1200

    extend func

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rustjblas;
 use rustjblas::matrix::DoubleMatrix;
 
 fn main() {
-    let m = DoubleMatrix::new(3, 3, vec![1.0, 4.0, 7.0, 4.0, 5.0, 8.0, 7.0, 8.0, 9.0]);
+    let m = DoubleMatrix::rand(6, 3);
     println!("{}", m);
     println!("is_square: {}", m.is_square());
     println!("is_symmetric: {}", m.is_symmetric());
@@ -11,4 +11,6 @@ fn main() {
     println!("is_row_vector: {}", m.is_row_vector());
     println!("is_column_vector: {}", m.is_column_vector());
     println!("{:?}", m);
+    println!("{}", format!("'{:>width$}'", "abc", width = 10));
+    m.show(true);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,7 @@
+extern crate rand;
+
 use std::fmt::{Display, Error, Formatter};
+use self::rand::Rng;
 
 #[derive(Debug)]
 pub struct DoubleMatrix {
@@ -12,6 +15,22 @@ impl DoubleMatrix {
         assert!(rows > 0 && cols > 0, "Expected {} rows and {} cols to be positive", rows, cols);
         assert!(data.len() == rows * cols, "len {} != {} rows * {} cols", data.len(), rows, cols);
         DoubleMatrix { rows, cols, data }
+    }
+
+    // create matrix with capacity of rows * cols, it is up to client to add values to the matrix
+    pub fn with_capacity(rows: usize, cols: usize) -> DoubleMatrix {
+        assert!(rows > 0 && cols > 0, "Expected {} rows and {} cols to be positive", rows, cols);
+        DoubleMatrix { rows, cols, data: Vec::with_capacity(rows * cols) }
+    }
+
+    pub fn rand(rows: usize, cols: usize) -> DoubleMatrix {
+        assert!(rows > 0 && cols > 0, "Expected {} rows and {} cols to be positive", rows, cols);
+        let mut rng = rand::thread_rng();
+        let mut data = Vec::with_capacity(rows * cols);
+        for _ in 0..data.capacity() {
+            data.push(rng.next_f64());
+        }
+        DoubleMatrix { rows, cols, data: data }
     }
 
     fn index(&self, i: usize, j: usize) -> usize {
@@ -33,6 +52,30 @@ impl DoubleMatrix {
     pub fn put(&mut self, i: usize, j: usize, value: f64) {
         let ind = self.index(i, j);
         self.data[ind] = value;
+    }
+
+    // replace data of the matrix with provided vector
+    pub fn replace(&mut self, data: Vec<f64>) {
+        assert!(
+            data.len() == self.rows * self.cols,
+            "len {} != {} rows * {} cols",
+            data.len(), self.rows, self.cols
+        );
+        self.data = data;
+    }
+
+    pub fn add_scalar(&mut self, value: f64) {
+        for ind in 0..self.data.len() {
+            self.data[ind] += value;
+        }
+    }
+
+    pub fn add_matrix(&mut self, matrix: &DoubleMatrix) {
+        assert!(self.rows == matrix.rows && self.cols == matrix.cols);
+        assert!(self.data.len() == matrix.data.len());
+        for ind in 0..self.data.len() {
+            self.data[ind] += matrix.data[ind];
+        }
     }
 
     pub fn is_square(&self) -> bool {
@@ -63,6 +106,58 @@ impl DoubleMatrix {
             }
         }
         true
+    }
+
+    // pretty print of the matrix
+    pub fn show(&self, truncate: bool) {
+        // convert each element into string and collect max width for each column
+        let row_bound = 7;
+        let col_bound = 3;
+
+        let truncated = truncate && (self.rows > row_bound || self.cols > col_bound);
+
+        let rows = if truncated { row_bound } else { self.rows };
+        let cols = if truncated { col_bound } else { self.cols };
+
+        let mut values = Vec::with_capacity(rows * cols);
+        for col in 0..cols {
+            for row in 0..rows {
+                values.push(format!("{:.4}", self.data[self.index(row, col)]));
+            }
+        }
+
+        // max widths for each column, each value is padded to this width
+        let mut col_size = Vec::with_capacity(self.cols);
+        for col in 0..cols {
+            let mut max_width = 0;
+            for row in 0..rows {
+                let value = values[row + rows * col].len();
+                if value > max_width {
+                    max_width = value;
+                }
+            }
+            col_size.push(max_width);
+        }
+
+        let mut matrix_str = String::new();
+        matrix_str.push_str(&format!("Matrix {} x {}:\n", self.rows, self.cols));
+
+        for i in 0..rows {
+            for j in 0..cols {
+                let value = &values[i + rows * j];
+                matrix_str.push_str(&format!("{:width$} ", value, width = col_size[j]));
+            }
+            if truncated {
+                matrix_str.push_str("...");
+            }
+            matrix_str.push('\n');
+        }
+
+        if truncated {
+            matrix_str.push_str("... (truncated)");
+        }
+
+        println!("{}", matrix_str);
     }
 }
 
