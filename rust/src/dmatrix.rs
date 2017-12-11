bf35ca71d50bf353f0f512be538e1411f4c415ab
sadikovi <ivan.sadikov@gmail.com>
2017-12-11 21:30:18 +1300

    update dmatrix

--- rust/src/dmatrix.rs
+++ rust/src/dmatrix.rs
@@ -1,10 +1,4 @@
 use nalgebra::{Dynamic, Matrix, MatrixVec, U1};
 
-// Dynamically sized and allocated float column vector
-pub type ColumnVector = Matrix<f32, Dynamic, U1, MatrixVec<f32, Dynamic, U1>>;
-
-// Dynamically sized and allocated float row vector
-pub type RowVector = Matrix<f32, U1, Dynamic, MatrixVec<f32, U1, Dynamic>>;
-
 // Dynamically sized and dynamically allocated float matrix
-pub type DoubleMatrix = Matrix<f32, Dynamic, Dynamic, MatrixVec<f32, Dynamic, Dynamic>>;
+pub type DoubleMatrix = Matrix<f64, Dynamic, Dynamic, MatrixVec<f64, Dynamic, Dynamic>>;
